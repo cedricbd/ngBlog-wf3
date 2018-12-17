461f834202a9f6d41a489d6cec44315d92d434da
cedricbd <=>
2018-12-17 14:34:33 +0100

    add date and compoment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  posts: Array<Object> = [
+    {
+    title: 'Mon premier post',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque laoreet posuere enim ut accumsan. Cras aliquam rutrum dui, a malesuada diam finibus quis. Donec vel massa pellentesque, facilisis urna cursus, suscipit orci. Sed non mattis lorem, eget efficitur nunc. Proin mattis non enim sed sagittis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In vel lectus vel lorem suscipit auctor eget ac turpis.',
+    loveIts: 0,
+    created_at: new Date()      
+    },
+    {
+    title: 'Mon deuxième post',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque laoreet posuere enim ut accumsan. Cras aliquam rutrum dui, a malesuada diam finibus quis. Donec vel massa pellentesque, facilisis urna cursus, suscipit orci. Sed non mattis lorem, eget efficitur nunc. Proin mattis non enim sed sagittis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In vel lectus vel lorem suscipit auctor eget ac turpis.',
+    loveIts: 0,
+    created_at: new Date() 
+  
+    },
+    {
+    title: 'Enfin, un dernier article',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque laoreet posuere enim ut accumsan. Cras aliquam rutrum dui, a malesuada diam finibus quis. Donec vel massa pellentesque, facilisis urna cursus, suscipit orci. Sed non mattis lorem, eget efficitur nunc. Proin mattis non enim sed sagittis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In vel lectus vel lorem suscipit auctor eget ac turpis.',
+    loveIts: 0,
+    created_at: new Date()
+    },
+  
+  ];
+  
+
   title = 'blog';
 }
--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-post-list',
@@ -7,28 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostListComponent implements OnInit {
 
-  posts: Array<Object> = [
-  {
-  title: 'Mon premier post',
-  content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque laoreet posuere enim ut accumsan. Cras aliquam rutrum dui, a malesuada diam finibus quis. Donec vel massa pellentesque, facilisis urna cursus, suscipit orci. Sed non mattis lorem, eget efficitur nunc. Proin mattis non enim sed sagittis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In vel lectus vel lorem suscipit auctor eget ac turpis.',
-  loveIts: 0,
-  created_at: new Date()      
-  },
-  {
-  title: 'Mon deuxième post',
-  content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque laoreet posuere enim ut accumsan. Cras aliquam rutrum dui, a malesuada diam finibus quis. Donec vel massa pellentesque, facilisis urna cursus, suscipit orci. Sed non mattis lorem, eget efficitur nunc. Proin mattis non enim sed sagittis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In vel lectus vel lorem suscipit auctor eget ac turpis.',
-  loveIts: 0,
-  created_at: new Date() 
-
-  },
-  {
-  title: 'Enfin, un dernier article',
-  content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque laoreet posuere enim ut accumsan. Cras aliquam rutrum dui, a malesuada diam finibus quis. Donec vel massa pellentesque, facilisis urna cursus, suscipit orci. Sed non mattis lorem, eget efficitur nunc. Proin mattis non enim sed sagittis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In vel lectus vel lorem suscipit auctor eget ac turpis.',
-  loveIts: 0,
-  created_at: new Date()
-  },
-
-];
+  @Input() posts
 
   constructor() { }
 
