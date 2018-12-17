1cea6a96191328437bc85c9bc8274c03b2da3f14
cedricbd <=>
2018-12-17 12:30:50 +0100

    passing data from post-it to post component

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostListComponent implements OnInit {
 
+  posts: Array<Object> = [
+  {
+  title: 'Mon premier post',
+  content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque laoreet posuere enim ut accumsan. Cras aliquam rutrum dui, a malesuada diam finibus quis. Donec vel massa pellentesque, facilisis urna cursus, suscipit orci. Sed non mattis lorem, eget efficitur nunc. Proin mattis non enim sed sagittis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In vel lectus vel lorem suscipit auctor eget ac turpis.',
+  love: 0,
+  created_at: new Date()      
+  },
+  {
+  title: 'Mon deuxième post',
+  content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque laoreet posuere enim ut accumsan. Cras aliquam rutrum dui, a malesuada diam finibus quis. Donec vel massa pellentesque, facilisis urna cursus, suscipit orci. Sed non mattis lorem, eget efficitur nunc. Proin mattis non enim sed sagittis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In vel lectus vel lorem suscipit auctor eget ac turpis.',
+  love: 0,
+  created_at: new Date() 
+
+  },
+  {
+  title: 'Enfin, un dernier article',
+  content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque laoreet posuere enim ut accumsan. Cras aliquam rutrum dui, a malesuada diam finibus quis. Donec vel massa pellentesque, facilisis urna cursus, suscipit orci. Sed non mattis lorem, eget efficitur nunc. Proin mattis non enim sed sagittis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In vel lectus vel lorem suscipit auctor eget ac turpis.',
+  love: 0,
+  created_at: new Date()
+  },
+
+];
+
   constructor() { }
 
   ngOnInit() {
